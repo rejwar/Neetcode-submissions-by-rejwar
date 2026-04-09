057e0dc13e34926181c2cf80acdcb44539c3fb8c
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2026-04-09 19:24:01 +0600

    Add: is-palindrome - submission-19

---
+++ Data Structures & Algorithms/is-palindrome/submission-19.rs
@@ -0,0 +1,70 @@
+
+
+// impl Solution {
+//     pub fn is_palindrome(s: String) ->bool {
+//         let clean = s.chars()
+//         .filter(|c|c.is_alphanumeric())
+//         .map(|c|c.to_ascii_lowercase());
+
+//         clean.clone().eq(clean.rev())
+//     }
+// }
+
+
+
+// impl Solution{
+//     pub fn  is_palindrome(s: String) -> bool {
+//         let clean = s.chars()
+//         .filter(|c|c.is_alphanumeric())
+//         .map(|c|c.to_ascii_lowercase());
+
+//         clean.clone().eq(clean.rev())
+// }
+// }
+
+
+
+// impl Solution{
+//     pub fn is _palindrome(s: String) -> bool {
+//         let clean =s.chars()
+//         .filter(|c|c.is_alphanumeric())
+//         .map(|c|c.to_ascii_lowecase());
+
+//         clean.clone().eq(clean.rec())
+//     }
+// }
+
+
+
+// impl Solution{
+//     pub fn is_palindrome(s: String) -> bool {
+//         let clean = s.chars()
+//         .filter(|c|c.is_alphanumeric())
+//         .map(|c|c.to_ascii_lowercase());
+
+//         clean.clone().eq(clean.rev())
+//     }
+// }
+
+
+// impl Solution {
+//     pub fn is_palindrome(s: String) -> bool {
+//         let clean =s.chars()
+//         .filter(|c|c.is_alphanumeric())
+//         .map(|c|c.to_ascii_lowercase());
+
+//         clean.clone().eq(clean.rev())
+//     }
+// }
+
+
+
+impl Solution {
+    pub fn is_palindrome (s: String) -> bool {
+        let clean = s.chars()
+        .filter(|c|c.is_alphanumeric())
+        .map(|c|c.to_ascii_lowercase());
+
+        clean.clone().eq(clean.rev())
+    }
+}
