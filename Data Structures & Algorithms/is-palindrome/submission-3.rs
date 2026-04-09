84b38310fed2d668ded1aba9e4c509fee434243c
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2026-04-09 19:09:08 +0600

    Update: is-palindrome - submission-3

--- Data Structures & Algorithms/is-palindrome/submission-3.rs
+++ Data Structures & Algorithms/is-palindrome/submission-3.rs
@@ -1,9 +1,11 @@
-impl Solution {
-    pub fn is_palindrome(s: String) -> bool {
-        let it = s.chars()
-            .filter(|c| c.is_alphanumeric())
-            .map(|c| c.to_ascii_lowercase());
 
-        it.clone().eq(it.rev())
+
+impl Solution {
+    pub fn is_palindrome(s: String) ->bool {
+        let clean = s.chars()
+        .filter(|c|c.is_alphanumeric())
+        .map(|c|c.to_ascii_lowercase())
+
+        clean.cloned().eq(clean.rev());
     }
 }
