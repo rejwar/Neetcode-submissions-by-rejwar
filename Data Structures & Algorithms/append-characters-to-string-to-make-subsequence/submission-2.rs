072ec7e913a21703ec0f11d57f42699cd6d846a7
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2026-04-18 06:24:47 +0600

    Add: append-characters-to-string-to-make-subsequence - submission-2

---
+++ Data Structures & Algorithms/append-characters-to-string-to-make-subsequence/submission-2.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn append_characters(s: String, t: String) -> i32 {
+        let s_bytes = s.as_bytes();
+        let t_bytes = t.as_bytes();
+        
+        let mut j = 0; 
+
+       
+        for &c in s_bytes {
+           
+            if j < t_bytes.len() && c == t_bytes[j] {
+                j += 1; 
+            }
+        }
+
+        (t_bytes.len() - j) as i32
+    }
+}
