4e116e44ce3f06f0cbc789afcc58149a618d8934
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2026-04-23 15:11:32 +0600

    Add: reverse-string - submission-0

---
+++ Data Structures & Algorithms/reverse-string/submission-0.rs
@@ -0,0 +1,14 @@
+impl Solution {
+    pub fn reverse_string(s: &mut Vec<char>) {
+        if s.is_empty() { return; }
+        
+        let mut l = 0;
+        let mut r = s.len() - 1;
+
+        while l < r {
+            s.swap(l, r); 
+            l += 1;      
+            r -= 1;      
+        }
+    }
+}
