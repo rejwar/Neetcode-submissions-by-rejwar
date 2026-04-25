f2bb177f8563c8173f50079e486df2c198fb8a20
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2026-04-25 20:17:45 +0600

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        if strs.is_empty() { return String::new(); }
+        
+        let mut prefix = strs[0].clone();
+        
+        for s in strs.iter().skip(1) {
+            while !s.starts_with(&prefix) {
+                prefix.pop();
+            }
+        }
+        
+        prefix
+    }
+}
