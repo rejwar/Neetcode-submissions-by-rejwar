a79510ee5a72bb4601cbae37e61abc12f1292291
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2026-05-08 19:53:47 +0600

    Add: range-sum-query-immutable - submission-0

---
+++ Data Structures & Algorithms/range-sum-query-immutable/submission-0.rs
@@ -0,0 +1,17 @@
+struct NumArray {
+    prefix: Vec<i32>,
+}
+
+impl NumArray {
+    fn new(nums: Vec<i32>) -> Self {
+        let mut prefix = vec![0; nums.len() + 1];
+        for i in 0..nums.len() {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+        NumArray { prefix }
+    }
+    
+    fn sum_range(&self, left: i32, right: i32) -> i32 {
+        self.prefix[(right + 1) as usize] - self.prefix[left as usize]
+    }
+}
