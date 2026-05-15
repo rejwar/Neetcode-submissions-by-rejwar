bd1a042e712d1e266b27142b457a1bbb2e5e1f06
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2026-05-15 20:24:02 +0600

    Add: find-lucky-integer-in-an-array - submission-4

---
+++ Data Structures & Algorithms/find-lucky-integer-in-an-array/submission-4.rs
@@ -0,0 +1,27 @@
+impl Solution {
+    pub fn find_lucky(arr: Vec<i32>) -> i32 {
+
+
+        let  n = arr.len();
+
+        for i in 0..n{
+
+            for j in i+1..n {
+
+                if arr[i] < arr[j]  
+                
+                {
+                    arr[i] == arr[i+1]; 
+                } else {
+                    arr[i] = arr[i];
+                }
+            }
+    let temp = arr[i];
+
+        }
+
+
+
+
+    }
+}
