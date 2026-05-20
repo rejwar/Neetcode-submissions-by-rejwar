b88d86f49c4cf762f3629e7e72b6f35827b1ad99
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2026-05-20 22:21:28 +0600

    Add: maximum-product-difference-between-two-pairs - submission-0

---
+++ Data Structures & Algorithms/maximum-product-difference-between-two-pairs/submission-0.rs
@@ -0,0 +1,26 @@
+impl Solution {
+    pub fn max_product_difference(nums: Vec<i32>) -> i32 {
+        let mut max1 = 0;
+        let mut max2 = 0;
+        let mut min1 = i32::MAX;
+        let mut min2 = i32::MAX;
+
+        for &n in &nums {
+            if n > max1 {
+                max2 = max1;
+                max1 = n;
+            } else if n > max2 {
+                max2 = n;
+            }
+
+            if n < min1 {
+                min2 = min1;
+                min1 = n;
+            } else if n < min2 {
+                min2 = n;
+            }
+        }
+
+        max1 * max2 - min1 * min2
+    }
+}
