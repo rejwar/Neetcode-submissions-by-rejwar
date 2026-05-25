647c95f6148d9726641eef07f9d8b140f98e632c
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2026-05-25 23:17:05 +0600

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+        let (mut left, mut right) = (0, nums.len() as i32 - 1);
+
+        while left <= right {
+            let mid = left + (right - left) / 2;
+            if nums[mid as usize] == target {
+                return mid;
+            } else if nums[mid as usize] < target {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        left
+    }
+}
