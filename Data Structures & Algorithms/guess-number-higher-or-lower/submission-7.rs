658edca3e23c0a8b7802e1c50f95e8ef7aeed4f7
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2026-06-10 12:38:20 +0600

    Add: guess-number-higher-or-lower - submission-7

---
+++ Data Structures & Algorithms/guess-number-higher-or-lower/submission-7.rs
@@ -0,0 +1,27 @@
+/** * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ * otherwise return 0
+ * unsafe fn guess(num: i32) -> i32 {}
+ */
+
+impl Solution {
+    #[allow(non_snake_case)]
+    pub unsafe fn guessNumber(n: i32) -> i32 {
+        let (mut left, mut right) = (1, n);
+
+        while left <= right {
+            let mid = left + (right - left) / 2;
+            
+            match guess(mid) {
+                0 => return mid,
+                -1 => right = mid - 1,
+                1 => left = mid + 1,
+                _ => unreachable!(),
+            }
+        }
+
+        -1
+    }
+}
