a8f5c2579e0cb8b1cbf14a6fde4a4b8b33c83d1d
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2026-06-11 20:51:55 +0600

    Add: binary-tree-diameter - submission-1

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-1.rs
@@ -0,0 +1,26 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn diameter_of_binary_tree(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let mut max_diameter = 0;
+        Self::dfs(&root, &mut max_diameter);
+        max_diameter
+    }
+
+    fn dfs(node: &Option<Rc<RefCell<TreeNode>>>, max_diameter: &mut i32) -> i32 {
+        let n = match node {
+            None => return 0,
+            Some(n) => n.borrow(),
+        };
+
+        let left_h  = Self::dfs(&n.left,  max_diameter);
+        let right_h = Self::dfs(&n.right, max_diameter);
+
+     
+        *max_diameter = (*max_diameter).max(left_h + right_h);
+
+       
+        1 + left_h.max(right_h)
+    }
+}
