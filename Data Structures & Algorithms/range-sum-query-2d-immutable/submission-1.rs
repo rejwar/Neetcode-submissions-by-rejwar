07bcc0df2315407a3d1d808aa30b0dce3ee689c8
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2026-06-21 18:43:46 +0600

    Add: range-sum-query-2d-immutable - submission-1

---
+++ Data Structures & Algorithms/range-sum-query-2d-immutable/submission-1.rs
@@ -0,0 +1,13 @@
+struct NumMatrix {
+
+}
+
+impl NumMatrix {
+    fn new(matrix: Vec<Vec<i32>>) -> Self {
+
+    }
+
+    fn sum_region(&self, row1: i32, col1: i32, row2: i32, col2: i32) -> i32 {
+
+    }
+}
