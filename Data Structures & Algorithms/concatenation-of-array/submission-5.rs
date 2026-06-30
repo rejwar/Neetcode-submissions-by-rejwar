9b5fbc6c53dfb8bff749a20486b7ce167e7f9dfd
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2026-06-30 20:27:10 +0600

    Add: concatenation-of-array - submission-5

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-5.rs
@@ -0,0 +1,6 @@
+impl Solution {
+    pub fn get_concatenation(nums: Vec<i32>) -> Vec<i32> {
+        // .repeat(2) creates a new Vec by repeating the contents of the slice twice
+        nums.repeat(2)
+    }
+}
