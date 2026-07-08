6d6ff53b4dc43c4957e83920c92d86bee883e188
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2026-07-08 18:15:37 +0600

    Add: guess-number-higher-or-lower - submission-15

---
+++ Data Structures & Algorithms/guess-number-higher-or-lower/submission-15.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub unsafe fn guess_number(n: i64) -> i32 {
+        let (mut left, mut right) = (1, n);
+        
+        while left <= right {
+            let mid = left + (right - left) / 2;
+            
+            match guess(mid) {
+                0 => return mid as i32,
+                -1 => right = mid - 1,
+                1 => left = mid + 1,
+                _ => unreachable!(),
+            }
+        }
+        
+        unreachable!()
+    }
+}
