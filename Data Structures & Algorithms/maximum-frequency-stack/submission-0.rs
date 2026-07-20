dfc7ac60a784d739611552a7717c49dcd3f7ba06
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2026-07-20 23:07:57 +0600

    Add: maximum-frequency-stack - submission-0

---
+++ Data Structures & Algorithms/maximum-frequency-stack/submission-0.rs
@@ -0,0 +1,17 @@
+struct FreqStack {
+
+}
+
+impl FreqStack {
+    pub fn new() -> Self {
+
+    }
+
+    pub fn push(&mut self, val: i32) {
+
+    }
+
+    pub fn pop(&mut self) -> i32 {
+
+    }
+}
