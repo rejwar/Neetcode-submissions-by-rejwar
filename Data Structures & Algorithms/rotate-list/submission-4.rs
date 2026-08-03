997106dfa2e42576208d95d3649ea651df1b701d
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2026-08-03 18:25:56 +0600

    Add: rotate-list - submission-4

---
+++ Data Structures & Algorithms/rotate-list/submission-4.rs
@@ -0,0 +1,33 @@
+impl Solution {
+    pub fn rotate_right(mut head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
+        if head.is_none() || k == 0 {
+            return head;
+        }
+
+        let mut len = 0;
+        let mut ptr = &head;
+        while let Some(node) = ptr {
+            len += 1;
+            ptr = &node.next;
+        }
+
+        let k = k % len;
+        if k == 0 {
+            return head;
+        }
+
+        let mut new_tail = &mut head;
+        for _ in 0..(len - k - 1) {
+            new_tail = &mut new_tail.as_mut().unwrap().next;
+        }
+
+        let mut new_head = new_tail.as_mut().unwrap().next.take();
+
+        let mut tail = &mut new_head;
+        while tail.is_some() {
+            tail = &mut tail.as_mut().unwrap().next;
+        }
+        *tail = head;
+
+        new_head
+    }}
