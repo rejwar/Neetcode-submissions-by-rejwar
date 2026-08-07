97cc46e26d34c68db0ac3a501ea373125a49511c
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2026-08-07 12:05:39 +0600

    Add: implement-queue-using-stacks - submission-2

---
+++ Data Structures & Algorithms/implement-queue-using-stacks/submission-2.rs
@@ -0,0 +1,32 @@
+struct MyQueue {
+
+}
+
+impl MyQueue {
+    pub fn new() -> Self {
+
+    }
+
+    pub fn push(&mut self, x: i32) {
+
+    }
+
+    pub fn pop(&mut self) -> i32 {
+
+    }
+
+    pub fn peek(&self) -> i32 {
+
+    }
+
+    pub fn empty(&self) -> bool {
+
+    }
+}
+
+// Your MyQueue object will be instantiated and called as such:
+// let obj = MyQueue::new();
+// obj.push(x);
+// let ret_2: i32 = obj.pop();
+// let ret_3: i32 = obj.peek();
+// let ret_4: bool = obj.empty();
