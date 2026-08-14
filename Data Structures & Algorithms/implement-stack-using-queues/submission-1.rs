d9e6a140944f226243d375108e9b2fb0f7a859f6
Md. Rejwar Rahman Rifat <144032839+rejwar@users.noreply.github.com>
2026-08-14 20:13:59 +0600

    Add: implement-stack-using-queues - submission-1

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-1.rs
@@ -0,0 +1,27 @@
+use std::collections::VecDeque;
+
+struct MyStack {
+
+}
+
+impl MyStack {
+    pub fn new() -> Self {
+
+    }
+
+    pub fn push(&mut self, x: i32) {
+
+    }
+
+    pub fn pop(&mut self) -> i32 {
+
+    }
+
+    pub fn top(&self) -> i32 {
+
+    }
+
+    pub fn empty(&self) -> bool {
+
+    }
+}
